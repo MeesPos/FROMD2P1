b53936b8fbf969be63d48157d89b6a5965476e6a
MeesPos <meespostma_@hotmail.com>
2020-10-02 15:10:20 +0200

    T/m video 11 (en 12-pre), volgende keer doorgaan met 12

--- BoekenJSON/js/boeken.js
+++ BoekenJSON/js/boeken.js
@@ -26,9 +26,11 @@ const boekenObject = {
 
             // Alle auteurs in een lijst zetten
             let auteurs = "";
-            boek.auteurs.forEach(auteur => {
+            boek.auteurs.forEach((auteur, index) => {
                 let tv = auteur.tussenvoegsel ? auteur.tussenvoegsel + " " : "";
-                let sep = " ";
+                let sep = ", ";
+                if(index >= boek.auteurs.length - 2) { sep = " en "; }
+                if(index >= boek.auteurs.length - 1) { sep = ""; }
                 auteurs += auteur.voornaam + " " + tv + auteur.achternaam + sep;
             })
 
@@ -37,13 +39,38 @@ const boekenObject = {
             htmlUitvoer += `<img class="boek__cover" src="${boek.cover}" alt="${titel}">`;
             htmlUitvoer += `<h3 class="boek__titel">${titel}</h3>`;
             htmlUitvoer += `<p class="boek__auteurs">${auteurs}</p>`
-            htmlUitvoer += `<span class="boek__uitgave">Datum van uitgave: ${boek.uitgave}</span>`
+            htmlUitvoer += `<span class="boek__uitgave">Datum van uitgave: ${this.datumOmzetten(boek.uitgave)}</span>`
             htmlUitvoer += `<span class="boek__ean"> EAN: ${boek.ean}</span>`
             htmlUitvoer += `<span class="boek__taal"> Taal: ${boek.taal}</span>`
             htmlUitvoer += `<span class="boek__pagina">Pagina's: ${boek.paginas}</span>`
-            htmlUitvoer += `<div class="boek__prijs">&euro; ${boek.prijs}</div>`
+            htmlUitvoer += `<div class="boek__prijs"> ${boek.prijs.toLocaleString('nl-NL', {currency: 'EUR', style: 'currency'})}</div>`
             htmlUitvoer += `</section>`;
         });
         boeken.innerHTML = htmlUitvoer;
+    },
+    datumOmzetten(datumString) {
+        let datum = new Date(datumString);
+        let jaar  = datum.getFullYear();
+        let maand = this.getMaandnaam(datum.getMonth());
+        return `${maand} ${jaar}`;
+    },
+    getMaandnaam(m) {
+        let maand = "";
+        switch(m) {
+            case 0 : maand = "januari"; break;
+            case 1 : maand = "februari"; break;
+            case 2 : maand = "maart"; break;
+            case 3 : maand = "april"; break;
+            case 4 : maand = "mei"; break;
+            case 5 : maand = "juni"; break;
+            case 6 : maand = "juli"; break;
+            case 7 : maand = "augustus"; break;
+            case 8 : maand = "september"; break;
+            case 9 : maand = "oktober"; break;
+            case 10 : maand = "november"; break;
+            case 11 : maand = "december"; break;
+            default : maand = m;
+        }
+        return maand;
     }
 }
