ff22b6bf975fc6744267fd24c577275dcec97882
MeesPos <meespostma_@hotmail.com>
2020-10-02 14:48:56 +0200

    Commit van video 8

--- BoekenJSON/js/boeken.js
+++ BoekenJSON/js/boeken.js
@@ -13,7 +13,6 @@ xhr.open('GET', 'boeken.json', true);
 xhr.send();
 
 const boekenObject = {
-
     // Hier wordt een eigenschap data aangemaakt (Regel 7)
     uitvoeren() {
         let htmlUitvoer = "";
@@ -24,7 +23,26 @@ const boekenObject = {
                 titel += boek.voortitel + " "
             }
             titel += boek.titel;
-            htmlUitvoer += `<h3>${titel}</h3>`
+
+            // Alle auteurs in een lijst zetten
+            let auteurs = "";
+            boek.auteurs.forEach(auteur => {
+                let tv = auteur.tussenvoegsel ? auteur.tussenvoegsel + " " : "";
+                let sep = " ";
+                auteurs += auteur.voornaam + " " + tv + auteur.achternaam + sep;
+            })
+
+            // HTML variable toevoegen
+            htmlUitvoer += `<section class="boek">`;
+            htmlUitvoer += `<img class="boek__cover" src="${boek.cover}" alt="${titel}">`;
+            htmlUitvoer += `<h3 class="boek__titel">${titel}</h3>`;
+            htmlUitvoer += `<p class="boek__auteurs">${auteurs}</p>`
+            htmlUitvoer += `<span class="boek__uitgave">Datum van uitgave: ${boek.uitgave}</span>`
+            htmlUitvoer += `<span class="boek__ean"> EAN: ${boek.ean}</span>`
+            htmlUitvoer += `<span class="boek__taal"> Taal: ${boek.taal}</span>`
+            htmlUitvoer += `<span class="boek__pagina">Pagina's: ${boek.paginas}</span>`
+            htmlUitvoer += `<div class="boek__prijs">&euro; ${boek.prijs}</div>`
+            htmlUitvoer += `</section>`;
         });
         boeken.innerHTML = htmlUitvoer;
     }
