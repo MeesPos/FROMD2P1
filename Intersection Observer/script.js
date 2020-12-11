fef3dd402fc5749f8722cd70c9bea22d42f07206
MeesPos <meespostma_@hotmail.com>
2020-12-11 12:41:38 +0100

    Opdracht af

--- Intersection Observer/script.js
+++ Intersection Observer/script.js
@@ -1,11 +1,15 @@
 let urls = document.querySelectorAll('nav a');
 let sections = document.querySelectorAll('section');
-const opties = {};
+
+const opties = {
+    treshold: 1.0
+};
+
 const verwerking = (entries, observer) => {
     entries.forEach( entry => {
-        // console.log(entry.target.id + " doorsnijdt " + entry.isIntersecting);
+        console.log(entry.target.parentNode.id + " doorsnijdt " + entry.isIntersecting);
         if ( entry.isIntersecting ) {
-            let link = zoekBijpassendeLink('#' + entry.target.id);
+            let link = zoekBijpassendeLink('#' + entry.target.parentNode.id);
             maakActief(link);
         }
     })
@@ -13,7 +17,10 @@ const verwerking = (entries, observer) => {
 
 let observer = new IntersectionObserver(verwerking, opties);
 
-observer.observe(sections[1]);
+// observer.observe(sections[1]);
+sections.forEach( sectie => {
+    observer.observe(sectie.getElementsByTagName('p')[0]);
+});
 
 // Functies die de class actief verwijderd
 const verwijderActief = () => {
